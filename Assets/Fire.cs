49d01ae41228221f669df8f3e2086704b7e63ccf
Richard Leinfellner <richard@modulo17.com>
2018-03-20 00:36:14 +0000

    Updated rotate clamp code

--- Assets/Fire.cs
+++ Assets/Fire.cs
@@ -6,34 +6,55 @@ public class Fire : MonoBehaviour {
 
 
     public GameObject BallPrefab;
-    Rigidbody2D mRB;
 
     public Transform FireFrom;
     public Transform FireBase;
 
     public float FireForce=10.0f;
+    public float RotateSpeed = 10.0f;
+
+    float mZBaseRotation;
+
+    float mZRotation=0.0f;
+
+    Rigidbody2D mRB;
+
 
     // Use this for initialization
     void Start () {
-        mRB = GetComponent<Rigidbody2D>();
         Debug.Assert(BallPrefab != null, "Ball Prefab missing");
         Debug.Assert(FireFrom != null || FireBase ==null, "FireTranform(s) missing");
+
+        mRB = GetComponent<Rigidbody2D>();
+        Debug.Assert(mRB != null, "Rigidbody2D missing");
+        mZBaseRotation = mRB.rotation;
+
     }
 
     // Update is called once per frame
-    void Update () {
-		if(Input.GetKeyDown(KeyCode.Space)) {
+
+    bool mFire=false;
+	private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            mFire = true;
+        }
+	}
+
+	void FixedUpdate () {
+        if(mFire) {
             GameObject tGO=Instantiate(BallPrefab, FireFrom.position, Quaternion.identity);
             Rigidbody2D tBallRB = tGO.GetComponent<Rigidbody2D>();
-            tBallRB.AddForce((FireFrom.position - FireBase.position).normalized * FireForce, ForceMode2D.Impulse);
+            tBallRB.AddForce((FireFrom.position - FireBase.position).normalized * FireForce * tBallRB.mass, ForceMode2D.Impulse);
+            mFire = false;
         }
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            transform.Rotate(0, 0, 1.0f);
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            mZRotation += RotateSpeed * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.Rotate(0, 0, -1.0f);
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            mZRotation -= RotateSpeed * Time.deltaTime;
         }
+        Debug.Log(mZRotation);
+        mZRotation = Mathf.Clamp(mZRotation, -55.0f, 55.0f);
+        mRB.rotation = mZBaseRotation+mZRotation;
     }
 }
