02e19fad86fe0173c32272ed04052c233d199ab3
Richard Leinfellner <richard@modulo17.com>
2018-07-04 08:32:57 +0100

    Updated with powerbar

---
+++ Assets/BlockTile.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+
+public class BlockTile : MonoBehaviour {
+    
+    Rigidbody2D     mRB;
+    SpriteRenderer  mSR;
+
+    public  bool Fixed=true;
+    public  enum Weights {
+        Light
+        ,Heavy
+        ,VeryHeavy
+        ,Fixed
+        ,Undefined
+    }
+
+    public  Weights Weight = Weights.Light;
+    private Weights mOldWeight = Weights.Undefined;
+
+
+    // Use this for initialization
+	void Start () {
+        mRB = GetComponent<Rigidbody2D>();
+        mSR = GetComponent<SpriteRenderer>();
+        SetWeigth(Weight);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        SetWeigth(Weight);
+	}
+
+    void    SetWeigth(Weights vNewWeight) {
+        if(vNewWeight!=mOldWeight) {
+            switch (vNewWeight) {
+                case Weights.Light:
+                    mRB.isKinematic = false;
+                    mRB.mass = 10.0f;
+                    break;
+
+                case Weights.Heavy:
+                    mRB.isKinematic = false;
+                    mRB.mass = 10.0f;
+                    break;
+
+                case Weights.VeryHeavy:
+                    mRB.isKinematic = false;
+                    mRB.mass = 10.0f;
+                    break;
+
+                case Weights.Fixed:
+                    mRB.isKinematic = true;
+                    mRB.mass = 1.0f;
+                    break;
+
+                default:
+                    Debug.LogError("Undefined Weight");
+                    return;
+
+            }
+            mOldWeight = Weight;
+            Weight = vNewWeight;
+        }
+    }
+
+
+    void OnCollisionEnter2D(Collision2D vCollision) {
+        Debug.LogFormat("{0:s} {1}",vCollision.gameObject.name,vCollision.relativeVelocity);
+    }
+}
--- Assets/Fire.cs
+++ Assets/Fire.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Fire : MonoBehaviour {
 
@@ -10,14 +11,19 @@ public class Fire : MonoBehaviour {
     public Transform FireFrom;
     public Transform FireBase;
 
-    public float FireForce=10.0f;
+    public float MaxFireForce=20.0f;
+    public float MinFireForce = 5.0f;
     public float RotateSpeed = 10.0f;
 
     float mZBaseRotation;
 
     float mZRotation=0.0f;
 
+    float mFireForce;
+
     Rigidbody2D mRB;
+
+    public  Image PowerMeterImage;
 
 
     // Use this for initialization
@@ -28,7 +34,8 @@ public class Fire : MonoBehaviour {
         mRB = GetComponent<Rigidbody2D>();
         Debug.Assert(mRB != null, "Rigidbody2D missing");
         mZBaseRotation = mRB.rotation;
-
+        mFireForce = MaxFireForce * 0.2f;
+        UpdatePower(0.0f);
     }
 
     // Update is called once per frame
@@ -38,13 +45,24 @@ public class Fire : MonoBehaviour {
         if (Input.GetMouseButtonDown(0)) {
             mFire = true;
         }
+        if(Input.GetKey(KeyCode.LeftArrow)) {
+            UpdatePower(10.0f * Time.deltaTime);
+            
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            UpdatePower(-10.0f * Time.deltaTime);
+        }
 	}
+
+    void    UpdatePower(float vDelta) {
+        mFireForce = Mathf.Clamp(mFireForce+vDelta, MinFireForce, MaxFireForce);
+    }
 
 	void FixedUpdate () {
         if(mFire) {
             GameObject tGO=Instantiate(BallPrefab, FireFrom.position, Quaternion.identity);
             Rigidbody2D tBallRB = tGO.GetComponent<Rigidbody2D>();
-            tBallRB.AddForce((FireFrom.position - FireBase.position).normalized * FireForce * tBallRB.mass, ForceMode2D.Impulse);
+            tBallRB.AddForce((FireFrom.position - FireBase.position).normalized * mFireForce * tBallRB.mass, ForceMode2D.Impulse);
             mFire = false;
         }
         if (Input.GetKey(KeyCode.UpArrow)) {
@@ -53,8 +71,11 @@ public class Fire : MonoBehaviour {
         if (Input.GetKey(KeyCode.DownArrow)) {
             mZRotation -= RotateSpeed * Time.deltaTime;
         }
-        Debug.Log(mZRotation);
         mZRotation = Mathf.Clamp(mZRotation, -55.0f, 55.0f);
         mRB.rotation = mZBaseRotation+mZRotation;
+    }
+
+    private void LateUpdate() {
+        PowerMeterImage.rectTransform.localScale = new Vector2(1.0f - (mFireForce / MaxFireForce), 1);
     }
 }
